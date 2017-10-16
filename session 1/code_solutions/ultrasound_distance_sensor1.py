1f904350ba547e24ed08c0fa635f7d21f2dbd29b
Till Hackler <till.hackler@gmx.com>
2017-10-16 14:27:16 +0100

    Remove extras.

--- session 1/code_solutions/ultrasound_distance_sensor1.py
+++
@@ -1,8 +0,0 @@
-from gpiozero import DistanceSensor
-from time import sleep
-
-sensor = DistanceSensor(echo=18, trigger=4, queue_len=1)
-print("RR")
-while True:
-    print('Distance: ', sensor.distance)
-    sleep(1)
